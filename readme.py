25bb56ff32a78fc33cd6bfb7199d64fdc724fb74
Travis Bumgarner <travis.bumgarner@gmail.com>
2025-01-22 20:12:48 -0600

    first commit

---
+++ readme.py
@@ -0,0 +1,10 @@
+https://micropython.org/download/ESP32_GENERIC
+
+
+screen /dev/tty.usbserial-0001 115200
+ampy --port /dev/tty.usbserial-0001 put main.py
+
+ampy --port /dev/tty.usbserial-0001 rm main.py
+
+Probably main.py 
+/
